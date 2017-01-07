b7a17f5a014e6e81b4507c85316f57a46b4deb45
vasylboliuk <vbboliuk@gmail.com>
2017-01-07 14:58:42 +0200

    Add basic tests for Basic Mathematical Operations

--- src/8kyu/BasicMathematicalOperations.js
+++ src/8kyu/BasicMathematicalOperations.js
@@ -20,3 +20,9 @@ function basicOp(operation, value1, value2)
   }
   return result;
 }
+
+console.log("Basic tests\n");
+console.log(basicOp('+', 4, 7)===11);
+console.log(basicOp('-', 15, 18)===-3);
+console.log(basicOp('*', 5, 5)===25);
+console.log(basicOp('/', 49, 7)===7);
