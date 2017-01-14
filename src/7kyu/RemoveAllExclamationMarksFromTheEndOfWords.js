76df068ad63ddc5c72eba671b4d76c0e3f948610
vasylboliuk <vbboliuk@gmail.com>
2017-01-14 18:50:42 +0200

    Add new algorithm 7kyu Remove Explamation Marks from The end of words

---
+++ src/7kyu/RemoveAllExclamationMarksFromTheEndOfWords.js
@@ -0,0 +1,50 @@
+/*
+Description:
+Remove all exclamation marks from the end of words. Words are separated by spaces in the sentence.
+Examples
+
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi Hi"
+remove("!!!Hi !!hi!!! !hi") === "!!!Hi !!hi !hi"
+*/
+function remove(s){
+  
+  var result = "";
+  var str = s.split(" ");
+  
+  for(var i=0; i<str.length; i++){
+    while(str[i].endsWith("!")){
+      str[i] = str[i].substring(0, str[i].length-1);
+    }
+    result = result.concat(str[i], " "); 
+    
+  }
+  return result.trim();
+  
+}
+
+function remove2(s){
+  return s.replace(/\b!+/g, '');  
+}
+
+function remove3(s){
+  return s.split(' ').map(function(element){
+    return element.replace(/!*$/g,'');
+  }).join(' ');
+  
+}
+
+console.log("Basic tests");
+assertEquals(remove("Hi!") , "Hi");
+assertEquals(remove("Hi!!!") ,"Hi");
+assertEquals(remove("!Hi") , "!Hi");
+assertEquals(remove("Hi! Hi!") , "Hi Hi");
+assertEquals(remove2("!!!Hi !!hi!!! !hi") , "!!!Hi !!hi !hi");
+
+
+function assertEquals(actual, expected){
+	console.log(actual===expected);
+}
